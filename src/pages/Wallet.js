a42a1d2b64f34760a2db8caadb5085c4c8b2a870
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-08-03 15:22:36 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -13,7 +13,7 @@ class Wallet extends React.Component {
     return (
       <>
         <Header />
-        <h2>Tabela de Despesas</h2>
+        <h2>Tabela  de Despesas</h2>
         <WalletForm />
         <Table />
       </>

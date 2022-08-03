71b92e030a5862b44d5dcb55e6e8e54cea78d616
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-08-03 01:06:53 -0300

    Requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Table extends Component {
+class Table extends Component {
   tableHeader = () => {
     const arrayHeader = [
-      'Descrição', 'Tag', 'Método de pagamento', 'Valor', 'Moeda',
-      'Câmbio utilizado', 'Valor convertido', 'Moeda de conversão', 'Editar/Excluir',
+      'Descrição',
+      'Tag',
+      'Método de pagamento',
+      'Valor',
+      'Moeda',
+      'Câmbio utilizado',
+      'Valor convertido',
+      'Moeda de conversão',
+      'Editar/Excluir',
     ];
     return arrayHeader.map((item) => <th key={ item }>{item}</th>);
-  }
+  };
+
+  tableBody = () => {
+    const { walletExpenses } = this.props;
+    return walletExpenses.map((expense) => {
+      const
+        {
+          currency,
+          description,
+          exchangeRates,
+          id,
+          method,
+          tag,
+          value,
+        } = expense;
+      const filterRares = Object.entries(exchangeRates)
+        .filter((entry) => entry.includes(currency));
+      const { ask, name } = filterRares[0][1];
+      const converted = Number(value) * Number(ask);
+      return (
+        <tr key={ id }>
+          <td>{description}</td>
+          <td>{tag}</td>
+          <td>{method}</td>
+          <td>{value}</td>
+          <td>{name}</td>
+          <td>{Number(ask).toFixed(2)}</td>
+          <td>{converted.toFixed(2)}</td>
+          <td>Real</td>
+        </tr>
+      );
+    });
+  };
 
   render() {
     return (
       <table className="table">
         <thead>
-          <tr>
-            {this.tableHeader()}
-          </tr>
+          <tr>{this.tableHeader()}</tr>
         </thead>
+        <tbody>{this.tableBody()}</tbody>
       </table>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  walletExpenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  walletExpenses: PropTypes.arrayOf(Object).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);

d02b44b6d043f50de5f38081dadaeb2648da2980
brunotomaz-dev <tomaz.bruno@gmail.com>
2022-10-23 15:11:02 -0300

    Atualização do nome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -8,11 +8,11 @@ import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 

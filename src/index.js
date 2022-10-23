6b69b1a57ce736fc380d4fee1a8b95e070a0d157
brunotomaz-dev <tomaz.bruno@gmail.com>
2022-10-23 15:13:36 -0300

    Atualização do nome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
